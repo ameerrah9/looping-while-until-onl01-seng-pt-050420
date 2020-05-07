a986aa7d46f3dd4ba660c94957ae0754047842f9
Ghameerah McCullers <mccullers.ghameerah@gmail.com>
2020-05-07 15:20:46 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,13 +1,3 @@
-def using_until
-  levitation_force = 6
-	until levitation_force == 10
-	  puts "Wingardium Leviosa"
-	  levitation_force+=
-	end
-end
-
-using_until
-
 def using_while
   levitation_force = 6
 	while levitation_force > 10
